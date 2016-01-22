1ebfafa887ba7c75045c835a1530510efa75a31e
Tandl-os <freb@student.chalmers.se>
2016-01-22 14:18:03 +0100

    added comment conserning switches

--- Trainspotting/src/Lab1.java
+++ Trainspotting/src/Lab1.java
@@ -1,7 +1,7 @@
 import TSim.*;
 
 public class Lab1 {
-
+// temporary comment, switch 1: 17,7 2: 15,9 3: 3,9 4: 3,11
   public Lab1(Integer speed1, Integer speed2) {
       TSimInterface tsi = TSimInterface.getInstance();
 
