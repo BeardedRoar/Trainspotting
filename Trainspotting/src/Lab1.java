207467405bdf86c81ac309e36bf20822b6289b84
BeardedRoar <joeljoel224@hotmail.com>
2016-01-27 13:51:44 +0100

    added comments about the paths

--- Trainspotting/src/Lab1.java
+++ Trainspotting/src/Lab1.java
@@ -1,14 +1,21 @@
 import TSim.*;
 
+import java.util.concurrent.Semaphore;
+
 public class Lab1 {
 
+    private final Semaphore[] tracks;
     private TSimInterface tsi = TSimInterface.getInstance();
 
-// temporary comment, switch 1: 17,7 2: 15,9 3: 3,9 4: 3,11
+    // temporary comment, switch 1: 17,7 2: 15,9 3: 3,9 4: 3,11
+    // short path: 2,3,4,6,7
+    // overtaking-paths: 1,5,8
   public Lab1(Integer speed1, Integer speed2) {
 
+      this.tracks = new Semaphore[8];
+
     try {
-        //tsi.setSpeed(1, speed1);
+        tsi.setSpeed(1, speed1);
         tsi.setSpeed(2, speed2);
 
         tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
