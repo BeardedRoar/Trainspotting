b09c8211146db2a031b0966137a8e6fe51bea9cf
BeardedRoar <joeljoel224@hotmail.com>
2016-02-01 22:25:02 +0100

    visual fixes

--- Trainspotting/src/Lab1.java
+++ Trainspotting/src/Lab1.java
@@ -113,7 +113,7 @@ public class Lab1 {
          * @param se the sensor-event to handle
          * @throws InterruptedException in case the thread is interrupted while the event is still being handled.
          */
-        private void handleSensorEvent(SensorEvent se) throws InterruptedException {
+        private void handleSensorEvent(SensorEvent se) throws InterruptedException, CommandException {
             // Each event is assumed to be caused by the train monitored by thread, as this should be the only event
             // it could access.
 
@@ -123,81 +123,76 @@ public class Lab1 {
                 // Each block represents one sensor or two parallel sensors, and is handled different depending on the
                 // direction of the train, as this dictates if and what track it has left or tries to enter.
                 int x = se.getXpos(); int y = se.getYpos();
-                try {
-                    if (x == 14 && (y == 7 || y == 8)) {
-                        // Parallel sensors on tracks 0, train either trying to enter or has just left track 1.
-                        if (downwards) {
-                            tryEnterSharedTrack(1, 17, 7,
-                                    y == 7 ? TSimInterface.SWITCH_RIGHT: TSimInterface.SWITCH_LEFT);
-                        } else {
-                            releaseIfPossible(1);
-                        }
-                    } else if (x == 11 && (y == 7 || y == 8)) {
-                        if (downwards) {
-                            releaseIfPossible(5);
-                        } else {
-                            tryEnterSharedTrack(5);
-                        }
-                    } else if (x == 14 && (y == 3 || y == 5)) {
-                        if (!downwards) {
-                            stopTrain();
-                            waitAtStop();
-                            resumeTrain();
-                        }
-                    } else if (x == 18 && y == 7) {
-                        if (downwards) {
-                            releaseIfPossible(0);
-                            tryEnterFastTrack(2,15,9,TSimInterface.SWITCH_RIGHT,TSimInterface.SWITCH_LEFT);
-                        } else {
-                            releaseIfPossible(2);
-                            tryEnterFastTrack(0,17,7,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
-                        }
-                    } else if (x == 12 && (y == 9 || y == 10)) {
-                        if (downwards) {
-                            releaseIfPossible(1);
-                        } else {
-                            tryEnterSharedTrack(1, 15, 9,
-                                    y == 10 ? TSimInterface.SWITCH_LEFT : TSimInterface.SWITCH_RIGHT);
-                        }
-                    } else if (x == 7 && (y == 9 || y == 10)) {
-                        if (downwards) {
-                            tryEnterSharedTrack(3, 4, 9,
-                                    y == 9 ? TSimInterface.SWITCH_LEFT : TSimInterface.SWITCH_RIGHT);
-                        } else {
-                            releaseIfPossible(3);
-                        }
-                    } else if (y == 5 && (x == 9 || x == 6)) {
-                        if (downwards) {
-                            tryEnterSharedTrack(5);
-                        } else {
-                            releaseIfPossible(5);
-                        }
-                    } else if (x == 1 && y == 10) {
-                        if (downwards) {
-                            releaseIfPossible(2);
-                            tryEnterFastTrack(4,3,11,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
-                        } else {
-                            releaseIfPossible(4);
-                            tryEnterFastTrack(2,4,9,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
-                        }
-                    } else if (x == 14 && (y == 13 || y == 11)) {
-                        if (downwards) {
-                            stopTrain();
-                            waitAtStop();
-                            resumeTrain();
-                        }
-
-                    } else if (x == 6 && (y == 13 || y == 11)) {
-                        if (downwards) {
-                            releaseIfPossible(3);
-                        } else {
-                            tryEnterSharedTrack(3, 3, 11,
-                                    y == 13 ? TSimInterface.SWITCH_RIGHT : TSimInterface.SWITCH_LEFT);
-                        }
+                if (x == 14 && (y == 7 || y == 8)) {
+                    // Parallel sensors on tracks 0, train either trying to enter or has just left track 1.
+                    if (downwards) {
+                        tryEnterCriticalSection(1, 17, 7,
+                                y == 7 ? TSimInterface.SWITCH_RIGHT: TSimInterface.SWITCH_LEFT);
+                    } else {
+                        releaseSemaphoreIfNeeded(1);
+                    }
+                } else if (x == 11 && (y == 7 || y == 8)) {
+                    if (downwards) {
+                        releaseSemaphoreIfNeeded(5);
+                    } else {
+                        tryEnterCriticalSection(5);
+                    }
+                } else if (x == 14 && (y == 3 || y == 5)) {
+                    if (!downwards) {
+                        stopTrain();
+                        waitAtStop();
+                        resumeTrain();
+                    }
+                } else if (x == 18 && y == 7) {
+                    if (downwards) {
+                        releaseSemaphoreIfNeeded(0);
+                        tryEnterFastTrack(2,15,9,TSimInterface.SWITCH_RIGHT,TSimInterface.SWITCH_LEFT);
+                    } else {
+                        releaseSemaphoreIfNeeded(2);
+                        tryEnterFastTrack(0,17,7,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
+                    }
+                } else if (x == 12 && (y == 9 || y == 10)) {
+                    if (downwards) {
+                        releaseSemaphoreIfNeeded(1);
+                    } else {
+                        tryEnterCriticalSection(1, 15, 9,
+                                y == 10 ? TSimInterface.SWITCH_LEFT : TSimInterface.SWITCH_RIGHT);
+                    }
+                } else if (x == 7 && (y == 9 || y == 10)) {
+                    if (downwards) {
+                        tryEnterCriticalSection(3, 4, 9,
+                                y == 9 ? TSimInterface.SWITCH_LEFT : TSimInterface.SWITCH_RIGHT);
+                    } else {
+                        releaseSemaphoreIfNeeded(3);
+                    }
+                } else if (y == 5 && (x == 9 || x == 6)) {
+                    if (downwards) {
+                        tryEnterCriticalSection(5);
+                    } else {
+                        releaseSemaphoreIfNeeded(5);
+                    }
+                } else if (x == 1 && y == 10) {
+                    if (downwards) {
+                        releaseSemaphoreIfNeeded(2);
+                        tryEnterFastTrack(4,3,11,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
+                    } else {
+                        releaseSemaphoreIfNeeded(4);
+                        tryEnterFastTrack(2,4,9,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
+                    }
+                } else if (x == 14 && (y == 13 || y == 11)) {
+                    if (downwards) {
+                        stopTrain();
+                        waitAtStop();
+                        resumeTrain();
                     }
 
-                } catch (CommandException e){
-                    e.printStackTrace();
+                } else if (x == 6 && (y == 13 || y == 11)) {
+                    if (downwards) {
+                        releaseSemaphoreIfNeeded(3);
+                    } else {
+                        tryEnterCriticalSection(3, 3, 11,
+                                y == 13 ? TSimInterface.SWITCH_RIGHT : TSimInterface.SWITCH_LEFT);
+                    }
                 }
             }
         }
@@ -211,7 +206,7 @@ public class Lab1 {
          * @throws CommandException if the train cannot be stopped or resumed.
          * @throws InterruptedException if the Thread is interrupted.
          */
-        private void tryEnterSharedTrack(int trackID) throws CommandException, InterruptedException {
+        private void tryEnterCriticalSection(int trackID) throws CommandException, InterruptedException {
             if (!tracks[trackID].tryAcquire()) {
                 stopTrain();
                 tracks[trackID].acquire();
@@ -234,7 +229,7 @@ public class Lab1 {
          * @throws CommandException
          * @throws InterruptedException
          */
-        private void tryEnterSharedTrack(int trackID, int switchX, int switchY, int dir)
+        private void tryEnterCriticalSection(int trackID, int switchX, int switchY, int dir)
                 throws CommandException, InterruptedException {
             if (!tracks[trackID].tryAcquire()) {
                 stopTrain();
@@ -245,6 +240,26 @@ public class Lab1 {
             } else {
                 heldSemaphores.add(trackID);
                 Lab1.this.tsi.setSwitch(switchX, switchY, dir);
+            }
+        }
+
+        /**
+         * The calling Thread tries to acquire the given Semaphore and if it is successful flips the given Switch so
+         * the train can enter the fast track. Otherwise it flips it to enter the slow track.
+         *
+         * @param trackID The number of the Semaphore corresponding to the fast track.
+         * @param switchX The X-coordinate of the Switch corresponding to the fast track.
+         * @param switchY The Y-coordinate of the Switch corresponding to the fast track.
+         * @param successDir The direction to flip the Switch to in order to enter the fast track.
+         * @param failDir The direction to flip the Switch to in order to enter the slow track.
+         * @throws CommandException if the Switch cannot be flipped.
+         */
+        private void tryEnterFastTrack(int trackID, int switchX, int switchY, int successDir, int failDir) throws CommandException {
+            if(tracks[trackID].tryAcquire()){
+                Lab1.this.tsi.setSwitch(switchX, switchY, successDir);
+                heldSemaphores.add(trackID);
+            } else {
+                Lab1.this.tsi.setSwitch(switchX, switchY,failDir);
             }
         }
 
@@ -285,32 +300,14 @@ public class Lab1 {
          *
          * @param semaphore The Semaphore to be released if held.
          */
-        private void releaseIfPossible(int semaphore){
+        private void releaseSemaphoreIfNeeded(int semaphore){
             if(heldSemaphores.contains(semaphore)) {
                 tracks[semaphore].release();
-                heldSemaphores.remove((Object) semaphore);
+                heldSemaphores.remove(semaphore);
             }
         }
 
-        /**
-         * The calling Thread tries to acquire the given Semaphore and if it is successful flips the given Switch so
-         * the train can enter the fast track. Otherwise it flips it to enter the slow track.
-         *
-         * @param trackID The number of the Semaphore corresponding to the fast track.
-         * @param switchX The X-coordinate of the Switch corresponding to the fast track.
-         * @param switchY The Y-coordinate of the Switch corresponding to the fast track.
-         * @param successDir The direction to flip the Switch to in order to enter the fast track.
-         * @param failDir The direction to flip the Switch to in order to enter the slow track.
-         * @throws CommandException if the Switch cannot be flipped.
-         */
-        private void tryEnterFastTrack(int trackID, int switchX, int switchY, int successDir, int failDir) throws CommandException {
-            if(tracks[trackID].tryAcquire()){
-                Lab1.this.tsi.setSwitch(switchX, switchY, successDir);
-                heldSemaphores.add(trackID);
-            } else {
-                Lab1.this.tsi.setSwitch(switchX, switchY,failDir);
-            }
-        }
+
 
     }
 }
