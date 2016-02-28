72c99650c5ec9ee70df5cd9a21510ace4b6d290f
Tandl-os <freb@student.chalmers.se>
2016-02-28 15:13:34 +0100

    added faulty connect

--- Cchat/server.erl
+++ Cchat/server.erl
@@ -18,8 +18,13 @@ initial_state(ServerName) ->
 %% {reply, Reply, NewState}, where Reply is the reply to be sent to the client
 %% and NewState is the new state of the server.
 
-handle(St, Request) ->
-    io:fwrite("Server received: ~p~n", [Request]),
-    Response = "hi!",
-    io:fwrite("Server is sending: ~p~n", [Response]),
-    {reply, Response, St}.
+%%handle(St, Request) ->
+%%    io:fwrite("Server received: ~p~n", [Request]),
+%%    Response = "hi!",
+%%    io:fwrite("Server is sending: ~p~n", [Response]),
+%%    {reply, Response, St};
+
+	handle(St, {connect, _ClientId, _Nick}) ->
+	NewDict = dict:store(_Nick, _ClientId, St#server_st.clients),
+	X = St#server_st{clients = NewDict},
+	{reply, ok, X}.
