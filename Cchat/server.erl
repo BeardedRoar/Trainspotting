b828125bb343dca05600ccf8986ba29348a12b1e
BeardedRoar <joeljoel224@hotmail.com>
2016-02-29 14:48:36 +0100

    cleaning up printlines

--- Cchat/server.erl
+++ Cchat/server.erl
@@ -22,7 +22,6 @@ handle(St, {connect, _ClientId, _Nick}) ->
 	Connected = lists:keyfind(_Nick, 1, St#server_st.clients ),
 	case Connected of 
 		false ->
-			io:fwrite("~p~n", [St#server_st.clients]),
 			X = St#server_st{clients = [{_Nick, _ClientId}|St#server_st.clients]},
 			Result = ok;
 		_else ->
@@ -36,8 +35,6 @@ handle(St, {disconnect, _ClientId, _Nick}) ->
 	{reply, ok, X};
 	
 handle(St, {join, _Nick, _ClientId, _Channel}) ->
-	io:fwrite("channels in serverstate are: ~p~n", [St#server_st.channels]),
-	io:fwrite("channel to match is: ~p~n", [_Channel]),
 	Channel = lists:keyfind(_Channel, #channel_st.name, St#server_st.channels),
 	case Channel of
 		false ->
@@ -48,12 +45,10 @@ handle(St, {join, _Nick, _ClientId, _Channel}) ->
 			NewChannelList = lists:keyreplace(_Channel, #channel_st.name, St#server_st.channels, NewChannel),
 			X = St#server_st{channels = NewChannelList}
 	end,
-	io:fwrite("channels are: ~p~n", [X]),
 	{reply, ok, X};
 		
 handle(St, {leave, _Nick, _ClientId, _Channel}) ->
 	Channel = lists:keyfind(_Channel, #channel_st.name, St#server_st.channels),
-	io:fwrite("Server received: ~p~n", [lists:keydelete(_Nick, 1, Channel#channel_st.clients )]),
 	NewChannel = Channel#channel_st{clients = lists:keydelete(_Nick, 1, Channel#channel_st.clients )},
 	NewChannelList = lists:keyreplace(_Channel, #channel_st.name, St#server_st.channels, NewChannel),
 	X = St#server_st{channels = NewChannelList},
@@ -63,15 +58,11 @@ handle(St, {leave, _Nick, _ClientId, _Channel}) ->
 	
 handle(St, {msg_from_GUI, _Channel, _Nick, _Msg}) ->
 	Channel = lists:keyfind(_Channel, #channel_st.name, St#server_st.channels),
-	io:fwrite("after delete are: ~p~n", [lists:keydelete(_Nick, 1, Channel#channel_st.clients )]),
 	Receivers = lists:keydelete(_Nick, 1, Channel#channel_st.clients ),
-	io:fwrite("recievers are: ~p~n", [Receivers]),
 	lists:foreach(fun(N) ->
 		genserver:request(element(2,N),{incoming_msg, _Channel, _Nick, _Msg}),
 							io:fwrite("N is: ~p~n", [element(2,N)])
 				end, Receivers),
-	%for each client in channel - client with nick Nick
-	%genserver:request(incoming message)
 	{reply, ok, St};
 	
 handle(St, Request) ->
